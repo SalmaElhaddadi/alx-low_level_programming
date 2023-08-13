b3a75cdf3532c1853280e7b9d1bc68a9a604f280
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-08-14 00:58:07 +0300

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point of the C program
+ * Return: program should return 0 if success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
